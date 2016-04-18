7286aa62bcbf23dd12fde2ded4cb6b0fd92b98ad
Muhammad Haris Khan <haris.khan@trivento.nl>
2016-04-18 10:24:06 +0200

    consistent css attributes

--- app.js
+++ app.js
@@ -8,15 +8,15 @@
       var responseParse = JSON.parse(this.responseText);
           articles = responseParse.results,
       //sort data by date publishedDate
-          filterByLatestDate = document.getElementById('filterByLatestDate'),
-          filterByOldestDate = document.getElementById('filterByOldestDate');
+          filterbyLatestDate = document.getElementById('filter-by-latestDate'),
+          filterByOldestDate = document.getElementById('filter-by-oldestdate');
       filterByOldestDate.addEventListener("click", function() {
         articles.sort(function(a, b){
           return new Date(a.publishedDate) - new Date(b.publishedDate);
         })
         DOM.addDOM(articles);
       })
-      filterByLatestDate.addEventListener("click", function() {
+      filterbyLatestDate.addEventListener("click", function() {
         articles.sort(function(a, b){
           return new Date(b.publishedDate) - new Date(a.publishedDate);
         })
